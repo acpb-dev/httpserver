9acc28e2ddc027a706f0cc0130ab1dbb41c7b26d
Bergy <53085640+acpb-dev@users.noreply.github.com>
2022-04-08 12:34:32 -0400

    Initial Commit

---
+++ src/Httpd.CLI/Program.cs
@@ -0,0 +1,14 @@
+﻿using Httpd;
+using System.Configuration;
+
+
+FileReader.ReadAppConfig();
+Console.WriteLine("My Httpd server!");
+var port = ConfigurationManager.AppSettings["port"];
+
+if (port.Equals(""))
+{
+    port = "3000";
+}
+var server = new Server(int.Parse(port));
+await server.Start();
